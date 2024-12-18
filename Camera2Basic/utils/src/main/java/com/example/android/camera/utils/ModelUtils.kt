5c6a240e2b072d1690d59cf870a27de50d4d5aeb
Akashdeep Singh <akashdeep.singh@stud-mail.uni-wuerzburg.de>
2024-12-18 15:02:17 +0100

    Fixing Models Utils error

---
+++ Camera2Basic/utils/src/main/java/com/example/android/camera/utils/ModelUtils.kt
@@ -0,0 +1,32 @@
+
+package com.example.android.camera.utils
+
+//package your.package.name.util
+
+import android.content.Context
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.nio.channels.FileChannel
+import java.nio.MappedByteBuffer
+import java.nio.ByteBuffer
+
+object ModelUtils {
+
+    fun loadModelFile(context: Context, fileName: String): ByteBuffer {
+        val assetManager = context.assets
+        val fileDescriptor = assetManager.openFd("simple_model.tflite")
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        return inputStream.channel.map(
+            FileChannel.MapMode.READ_ONLY,
+            fileDescriptor.startOffset,
+            fileDescriptor.declaredLength
+        ).also {
+            inputStream.close()
+        }
+    }
+
+    fun createInterpreter(context: Context, modelName: String): Interpreter {
+        val modelBuffer = loadModelFile(context, modelName)
+        return Interpreter(modelBuffer)
+    }
+}
